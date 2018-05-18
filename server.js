b3e9339d3ad87c2c270fe5697ed826588327dada
brianramaswami <brianramaswami@yahoo.com>
2018-05-18 14:24:38 -0700

    restyled the icons

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ require("./app/routing/htmlRoutes")(app);
 // The below code effectively "starts" our server
 // ==============================================================================
 
+
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
